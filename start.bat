e095f82064511f8a58eced301dc0920c328d84e7
Matheus Leal Leone <matheusleal.leone@gmail.com>
2023-06-26 18:45:35 -0500

    fix for `start.bat`

--- start.bat
+++ start.bat
@@ -1,15 +1,22 @@
 @echo off
 
+set "current_path=%CD%"
 if exist ".venvAuto_email" (
     echo Virtual environment already exists. Skipping requirement installation.
+    cd .venvAuto_email\Scripts
+    call activate.bat
 ) else (
+    echo Creating virtual environment, please wait...
     python -m venv .venvAuto_email
+    echo Virtual environment created sucessefull.
+    cd .venvAuto_email\Scripts
+    call activate.bat
+    cd %current_path%
     pip install -r requirements.txt
 )
 
-cd .venvAuto_email\Scripts
-call Activate.bat
-cd ..
-cd ..
+cd %current_path%
 python auto_email.py
-call Deactivate.bat
+call deactivate.bat
+echo Done. You may close this now.
+pause
--- start.sh
+++ start.sh
@@ -4,9 +4,9 @@ if [ -d ".venvAuto_email" ]; then
     echo "Virtual environment already exists. Skipping requirement installation."
 else
     python -m venv .venvAuto_email
+    source .venvAuto_email/bin/activate
     pip install -r requirements.txt
 fi
 
-source .venvAuto_email/bin/activate
 python auto_email.py
 deactivate
